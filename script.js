f7fa5c4032fdd81394c483dbf83f00ab2d0e2550
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-17 00:20:11 +0100

    add fahrenheit function

---
+++ script.js
@@ -0,0 +1,13 @@
+// Fahrenheit function
+
+let Fahrenheit = parseInt(prompt( " input your Fahrenheit  " ));
+let x = (32);
+let y = (1.8);
+
+    function sub ( Fahrenheit, x, y ){
+    return (Fahrenheit-x)/y
+}
+// invoke function
+alert(sub(Fahrenheit, x, y));
+console.log(sub(Fahrenheit, x, y));
+document.write(sub(Fahrenheit, x, y));
