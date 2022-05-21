797d26e187d0c6e2c208b782a8979cc8a677fdf6
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-21 12:40:25 +0100

    add Gallon

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-// Fahrenheit function
+// Celsius function
 
 let Fahrenheit = parseInt(prompt( "input your Fahrenheit number" ));
 let x = (32);
 let y = (1.8);
 
-    function sub( Fahrenheit, x, y ){
+    function Celsius( Fahrenheit, x, y ){
     return (Fahrenheit-x)/y
 }
 
-alert(sub(Fahrenheit, x, y));
-console.log(sub(Fahrenheit, x, y));
-document.write(sub(Fahrenheit, x, y));
+alert(Celsius(Fahrenheit, x, y));
+console.log(Celsius(Fahrenheit, x, y));
+document.write(Celsius(Fahrenheit, x, y));
+
+
+// Gallon function
+
+let A = parseInt(prompt( "input your gallon value" ));
+let B = (3.7854117842)
+
+ function Gallon( A,B ){
+    return A*B
+}
+
+alert(Gallon(A,B));
+console.log(Gallon( A,B));
+document.write(Gallon(A, B ));
+
+// Litter function
+
+let A = parseInt(prompt("input value of litter"));
+let B = 0.26417205236
+
+function litter ( A, B)
